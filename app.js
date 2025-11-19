43369d440e1d506fb5e4f588dfc3eebb1741f7ee
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2025-11-19 18:15:23 -0500

    added proper error loggin

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ app.use("/tracks", tracksRouter);
 app.use("/playlists", playlistsRouter);
 
 app.use((err, req, res, next) => {
+  // A switch statement can be used instead of if statements
+  // when multiple cases are handled the same way.
+  switch (err.code) {
+    // Invalid type
+    case "22P02":
+      return res.status(400).send(err.message);
+    // Unique constraint violation
+    case "23505":
+    // Foreign key violation
+    case "23503":
+      return res.status(400).send(err.detail);
+    default:
+      next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send("Sorry! Something went wrong.");
 });
